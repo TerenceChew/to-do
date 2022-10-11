b7c1abac08a082ed81fec3c4027be14f4fe9957a
TerenceChew <wschew97@gmail.com>
2022-10-11 07:10:48 -0400

    Add and set up files

---
+++ src/container/app.js
@@ -0,0 +1,9 @@
+import './app.css';
+
+export default function app() {
+  const container = document.createElement('div');
+
+  container.append('container');
+
+  return container;
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import app from './container/app';
+import './index.css';
+
+const root = document.querySelector('#root');
+
+root.append(app());
