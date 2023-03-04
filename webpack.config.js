cf5e984ba74837e00c8c04ca91139d9f08702de3
TerenceChew <wschew97@gmail.com>
2023-03-04 06:17:26 -0500

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,37 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(svg|jpeg|jpg|png|gif)$/i,
-        type: "asset/resource"
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: "index.html",
-      template: "./src/template.html"
-    })
+      template: "./src/template.html",
+      favicon: "./src/favicon.ico",
+    }),
   ],
   devtool: "inline-source-map",
   mode: "production",
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
-    maxAssetSize: 512000
- }
-}
+    maxAssetSize: 512000,
+  },
+};
